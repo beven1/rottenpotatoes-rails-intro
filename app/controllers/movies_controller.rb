94f5e78763be22c4e528c59fe944fadef9a59301
Brandon Even <brandon.even@gmail.com>
2018-09-24 21:39:41 -0600

    All components

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,9 +11,13 @@ class MoviesController < ApplicationController
   end
 
   def index
-    # @all_ratings = Movie.ratings
-    @sort = params[:sort]
-    @movies = Movie.all.order(@sort)
+    @all_ratings = Movie.ratings
+    @sort = params[:sort] || session[:sort]
+    session[:ratings] = session[:ratings] || @all_ratings
+    params[:ratings].nil? ? @t_param = @all_ratings : @t_param = params[:ratings].keys
+    session[:sort] = @sort
+    session[:ratings] = @t_param
+    @movies = Movie.where(rating: session[:ratings]).order(session[:sort])
   end
 
   def new
